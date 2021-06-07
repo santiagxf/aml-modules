fb5ca96da4f1effedbc2e4741b8dfe8d5a070bee
santiagxf <fasantia@microsoft.com>
2021-06-07 13:05:38 -0300

    Enabling testing frameworks to run

--- aml-extract-pca/extract_pca.py
+++ aml-extract-pca/extract_pca.py
@@ -4,17 +4,16 @@ import pandas as pd
 
 from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
 
-parser = argparse.ArgumentParser("extract-pca")
-parser.add_argument("--dataset", dest="input_dataset", required=True, type=str, help="Input dataset")
-parser.add_argument("--number-of-dimensions", dest="number_of_dimensions", type=int, help="Number of dimensions to reduce to", required=True)
-parser.add_argument("--normalize", dest="normalize", type=bool, help="Whether or not to normalize data to zero mean", required=True, default=True)
-parser.add_argument("--output-dataset", dest="output_dataset", type=str, help="Transformed dataset")
-parser.add_argument("--output-model", dest="output_model", type=str, help="Trained model")
-args = parser.parse_args()
+def RunModule(input_dataset: str, number_of_dimensions: int, normalize: bool, output_dataset: str, output_model: str):
+    pass
 
-input_dataset = args.input_dataset
-number_of_dimensions = args.number_of_dimensions
-normalize = args.normalize
-output_dataset = args.output_dataset
-output_model = args.output_model
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("extract-pca")
+    parser.add_argument("--dataset", dest="input_dataset", required=True, type=str, help="Input dataset")
+    parser.add_argument("--number-of-dimensions", dest="number_of_dimensions", type=int, help="Number of dimensions to reduce to", required=True)
+    parser.add_argument("--normalize", dest="normalize", type=bool, help="Whether or not to normalize data to zero mean", required=True, default=True)
+    parser.add_argument("--output-dataset", dest="output_dataset", type=str, help="Transformed dataset")
+    parser.add_argument("--output-model", dest="output_model", type=str, help="Trained model")
+    args = parser.parse_args()
 
+    RunModule(**args)
--- aml-promote-metrics/promote-metrics.py
+++ aml-promote-metrics/promote-metrics.py
@@ -10,60 +10,57 @@ COMPARE_SMALLER_BETTER = 'Smaller is better'
 PROMOTE_ALL_MODELS = 'All models metrics'
 PROMOTE_BEST_MODEL = 'Best model metrics'
 
-parser = argparse.ArgumentParser("promote-metrics")
-parser.add_argument("--evaluation-results", dest="evaluation_results", required=True, type=str, help="Evaluation results")
-parser.add_argument("--promote-method", dest="promote_method", type=str, choices= [PROMOTE_BEST_MODEL, PROMOTE_ALL_MODELS], required=False, default=PROMOTE_BEST_MODEL)
-parser.add_argument("--compare-by", dest="compare_by", type=str, help="Name of the metrics to compared against", required=False, default='Accuracy' )
-parser.add_argument("--compare-by-logic", dest="compare_by_logic", type=str, choices=[COMPARE_BIGGER_BETTER, COMPARE_SMALLER_BETTER], required=False, default=COMPARE_BIGGER_BETTER)
-parser.add_argument("--models-name", dest="models_name", type=str, required=False, default=None)
-parser.add_argument("--promoted-metrics", dest="promoted_metrics", type=str, help="Promotion results")
-args = parser.parse_args()
+def RunModule(evaluation_results: str, promote_method: str, compare_by: str, compare_by_logic: str, models_name: str, promoted_metrics: str):
+    # Load the metrics as a Pandas dataframe
+    results = load_data_frame_from_directory(evaluation_results).data
 
-evaluation_results = args.evaluation_results
-promote_method = args.promote_method
-compare_by = args.compare_by
-compare_by_logic = args.compare_by_logic
-models_name = args.models_name
-promoted_metrics = args.promoted_metrics
+    # Get the parent run
+    parent_run = Run.get_context().parent
 
-# Load the metrics as a Pandas dataframe
-results = load_data_frame_from_directory(evaluation_results).data
+    # Check if the metric exists in the available metrics
+    if compare_by not in results.columns:
+        print(f'Failed to find {compare_by} in available metrics. Using the first one')
+        compare_by = results.columns[0]
 
-# Get the parent run
-parent_run = Run.get_context().parent
-
-# Check if the metric exists in the available metrics
-if compare_by not in results.columns:
-    print('Failed to find', compare_by, 'in available metrics. Using the first one')
-    compare_by = results.columns[0]
-
-if models_name:
-    models_name = [name.strip() for name in models_name.split(',')]
-else:
-    # Generate models names like 'model A', 'model B' on the fly
-    models_name = [f"model {chr(65 + index)}" for index in range(0, results.shape[0])]
-
-# Filter the rows based on the metric you are looking for to compare and the
-# logic to compare
-if promote_method == PROMOTE_BEST_MODEL:
-    if compare_by_logic == COMPARE_BIGGER_BETTER:
-        results = results.loc[results[compare_by].argmax()]
+    if models_name:
+        models_name = [name.strip() for name in models_name.split(',')]
     else:
-        results = results.loc[results[compare_by].argmin()]
+        # Generate models names like 'model A', 'model B' on the fly
+        models_name = [f"model {chr(65 + index)}" for index in range(0, results.shape[0])]
 
-metrics = results.to_dict()
-for metric, value in metrics.items():
-    if type(value) is dict:
-        for model, point in value.items():
-            if len(value) > 1:
-                parent_run.log(name=f"{metric} ({models_name[model]})", value=point)
-            else:
-                parent_run.log(name=f"{metric}", value=point)
+    # Filter the rows based on the metric you are looking for to compare and the
+    # logic to compare
+    if promote_method == PROMOTE_BEST_MODEL:
+        if compare_by_logic == COMPARE_BIGGER_BETTER:
+            results = results.loc[results[compare_by].argmax()]
+        else:
+            results = results.loc[results[compare_by].argmin()]
+
+    metrics = results.to_dict()
+    for metric, value in metrics.items():
+        if type(value) is dict:
+            for model, point in value.items():
+                if len(value) > 1:
+                    parent_run.log(name=f"{metric} ({models_name[model]})", value=point)
+                else:
+                    parent_run.log(name=f"{metric}", value=point)
+        else:
+            parent_run.log(name=f"{metric}", value=value)
+
+    # Save the promoted metrics
+    if (type(results) is pd.DataFrame):
+        save_data_frame_to_directory(promoted_metrics, data=results)
     else:
-        parent_run.log(name=f"{metric}", value=value)
+        print("Skipping saving since the filtered data is not a valid Pandas.DataFrame")
 
-# Save the promoted metrics
-if (type(results) is pd.DataFrame):
-    save_data_frame_to_directory(promoted_metrics, data=results)
-else:
-    print("Skipping saving since the filtered data is not a valid Pandas.DataFrame")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("promote-metrics")
+    parser.add_argument("--evaluation-results", dest="evaluation_results", required=True, type=str, help="Evaluation results")
+    parser.add_argument("--promote-method", dest="promote_method", type=str, choices= [PROMOTE_BEST_MODEL, PROMOTE_ALL_MODELS], required=False, default=PROMOTE_BEST_MODEL)
+    parser.add_argument("--compare-by", dest="compare_by", type=str, help="Name of the metrics to compared against", required=False)
+    parser.add_argument("--compare-by-logic", dest="compare_by_logic", type=str, choices=[COMPARE_BIGGER_BETTER, COMPARE_SMALLER_BETTER], required=False, default=COMPARE_BIGGER_BETTER)
+    parser.add_argument("--models-name", dest="models_name", type=str, required=False, default=None)
+    parser.add_argument("--promoted-metrics", dest="promoted_metrics", type=str, help="Promotion results")
+    args = parser.parse_args()
+
+    RunModule(**args)
