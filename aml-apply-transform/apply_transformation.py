1fa2bb579d3f9db4d6b0a1756921a01b15817df5
santiagxf <fasantia@microsoft.com>
2021-06-08 11:45:00 -0300

    feat: apply transformation module

---
+++ aml-apply-transform/apply_transformation.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
+from azureml.studio.core.io.transformation_directory import PickleTransformationDirectory
+
+
+def RunModule(input_dataset: str, transformation: str, output_dataset: str):
+    data_folder = load_data_frame_from_directory(input_dataset)
+    data = data_folder.data
+    tranformation = PickleTransformationDirectory(transformation).load(Path(transformation))
+
+    transformed_data = tranformation.transform.transform(data)
+
+    if transformed_data.shape[-1] == data.shape[-1]:
+        column_names = data.columns
+    else:
+        column_names = [f"col{index}" for index in range(0, transformed_data.shape[-1])]
+
+    df = pd.DataFrame(data = transformed_data, columns = column_names)
+    save_data_frame_to_directory(output_dataset, df)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("extract-pca")
+    parser.add_argument("--dataset", dest="input_dataset", required=True, type=str, help="Input dataset")
+    parser.add_argument("--transformation", dest="transformation", type=str, help="Transformation with Scikit-learn transformation API")
+    parser.add_argument("--output-dataset", dest="output_dataset", type=str, help="Transformed dataset")
+    args = parser.parse_args()
+
+    RunModule(**vars(args))
--- aml-extract-pca/extract_pca.py
+++ aml-extract-pca/extract_pca.py
@@ -14,8 +14,11 @@ def RunModule(input_dataset: str, number_of_dimensions: int, normalize: bool, so
               output_dataset: str, output_model: str, output_eigenvectors: str):
     data_folder = load_data_frame_from_directory(input_dataset)
 
-    if any([col['type'] != 'Numeric' for col in data_folder.schema['columnAttributes']]):
-        raise TypeError('Dataset contains non-numeric columns. Cannot apply PCA to non-numeric types.')
+    if data_folder.schema:
+        if any([col['type'] != 'Numeric' for col in data_folder.schema['columnAttributes']]):
+            raise TypeError('Dataset contains non-numeric columns. Cannot apply PCA to non-numeric types.')
+    else:
+        print('Column type checking ignored as schema not available.')
     
     if number_of_dimensions <= 0:
         raise ValueError('The number of components cannot be less or equal to zero.')
@@ -37,7 +40,7 @@ def RunModule(input_dataset: str, number_of_dimensions: int, normalize: bool, so
 
     tranformations_pipe = Pipeline(tranformations)
 
-    components_name = [f"Component{chr(65 + index)}" for index in range(0, number_of_dimensions)]
+    components_name = [f"col{index}" for index in range(0, number_of_dimensions)]
     df = pd.DataFrame(data = transformed_data, columns = components_name)
 
     save_data_frame_to_directory(output_dataset, df)
