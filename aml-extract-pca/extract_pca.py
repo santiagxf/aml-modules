47823c4ca789e091b43fb81f5d4354dc51cc2f7c
santiagxf <fasantia@microsoft.com>
2021-06-08 09:47:23 -0300

    feat: PCA module

--- aml-extract-pca/extract_pca.py
+++ aml-extract-pca/extract_pca.py
@@ -1,27 +1,61 @@
 import argparse
+import pandas as pd
 from numpy.core.fromnumeric import partition
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
 from azureml.studio.core.io.transformation_directory import save_pickle_transform_to_directory
 
+PCA_SOLVERS = ['auto', 'full', 'arpack', 'randomized']
 
-def RunModule(input_dataset: str, number_of_dimensions: int, normalize: bool, output_dataset: str, output_model: str):
-    data = load_data_frame_from_directory(input_dataset).data
+def RunModule(input_dataset: str, number_of_dimensions: int, normalize: bool, solver: str, 
+              output_dataset: str, output_model: str, output_eigenvectors: str):
+    data_folder = load_data_frame_from_directory(input_dataset)
 
-    pca_transform = PCA(n_components=number_of_dimensions, svd_solver='randomized', whiten=True).fit(data)
-    transformed_data = pca_transform.transform(data)
+    if any([col['type'] != 'Numeric' for col in data_folder.schema['columnAttributes']]):
+        raise TypeError('Dataset contains non-numeric columns. Cannot apply PCA to non-numeric types.')
+    
+    if number_of_dimensions <= 0:
+        raise ValueError('The number of components cannot be less or equal to zero.')
 
-    save_data_frame_to_directory(output_dataset, transformed_data)
-    save_pickle_transform_to_directory(output_model, pca_transform)
+    if solver not in PCA_SOLVERS:
+        raise ValueError(f'Solver {solver} is not a valid PCA solver. Possible values are {PCA_SOLVERS}')
+
+    data = data_folder.data
+    tranformations = []
+
+    if normalize:
+        scaler = StandardScaler().fit(data)
+        data = scaler.transform(data)
+        tranformations.append(('normalize', scaler))
+
+    pca = PCA(n_components=number_of_dimensions, svd_solver=solver, whiten=True).fit(data)
+    transformed_data = pca.transform(data)
+    tranformations.append(('pca', pca))
+
+    tranformations_pipe = Pipeline(tranformations)
+
+    components_name = [f"Component{chr(65 + index)}" for index in range(0, number_of_dimensions)]
+    df = pd.DataFrame(data = transformed_data, columns = components_name)
+
+    save_data_frame_to_directory(output_dataset, df)
+    save_pickle_transform_to_directory(output_model, tranformations_pipe)
+
+    if output_eigenvectors:
+        eigenvectors = pd.DataFrame(data = pca.components_.T, columns = components_name)
+        save_data_frame_to_directory(output_eigenvectors, eigenvectors)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("extract-pca")
     parser.add_argument("--dataset", dest="input_dataset", required=True, type=str, help="Input dataset")
     parser.add_argument("--number-of-dimensions", dest="number_of_dimensions", type=int, help="Number of dimensions to reduce to", required=True)
     parser.add_argument("--normalize", dest="normalize", type=bool, help="Whether or not to normalize data to zero mean", required=True, default=True)
+    parser.add_argument("--solver", dest="solver", type=str, choices=PCA_SOLVERS, help="Solver to calculate PCA")
     parser.add_argument("--output-dataset", dest="output_dataset", type=str, help="Transformed dataset")
     parser.add_argument("--output-model", dest="output_model", type=str, help="Trained model")
+    parser.add_argument("--output-eigenvectors", dest="output_eigenvectors", required=False, type=str, help="Eigenvectors values")
     args = parser.parse_args()
 
     RunModule(**vars(args))
