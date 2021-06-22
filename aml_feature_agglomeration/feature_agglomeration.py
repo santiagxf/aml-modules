6752b46a034cc0e5d1c0567337d8067492c86340
santiagxf <fasantia@microsoft.com>
2021-06-22 15:46:26 -0300

    feat: feature agglomeration module

---
+++ aml_feature_agglomeration/feature_agglomeration.py
@@ -0,0 +1,95 @@
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import FeatureAgglomeration
+from sklearn.feature_extraction.image import grid_to_graph
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from scipy.cluster.hierarchy import dendrogram
+
+from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
+from azureml.studio.core.io.transformation_directory import save_pickle_transform_to_directory
+
+CONNECTIVITY_TYPE_NONE = 'none'
+CONNECTIVITY_TYPE_KNN = 'knn'
+CONNECTIVITY_TYPE_GRID = 'grid'
+AFFINITY_EUCLIDEAN = 'euclidean'
+CONNECTIVITY_TYPE = [CONNECTIVITY_TYPE_NONE, CONNECTIVITY_TYPE_KNN, CONNECTIVITY_TYPE_GRID]
+AFFINITY = [AFFINITY_EUCLIDEAN, 'l1', 'l2', 'manhattan', 'cosine']
+
+
+def plot_dendrogram(model, file_name:str, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    plt.figure(figsize=(10,10))
+    dendrogram(linkage_matrix, **kwargs)
+    plt.savefig(file_name, format='png', bbox_inches='tight')
+
+def RunModule(input_dataset: str, number_of_features: int, normalize: bool, connectivity_type: str,
+              affinity:str, output_dataset: str, output_model: str):
+
+    data_folder = load_data_frame_from_directory(input_dataset)
+    data = data_folder.data
+    tranformations = []
+    connectivity = None
+
+    if number_of_features <= 0:
+        raise ValueError('The number of components cannot be less or equal to zero.')
+
+    if data.shape[-1] >= number_of_features:
+        raise ValueError(f'The number of components ({number_of_features}) should be smaller than the number of features ({data.shape[-1]})')
+ 
+    if normalize:
+        scaler = StandardScaler().fit(data)
+        data = scaler.transform(data)
+        tranformations.append(('normalize', scaler))
+
+    if connectivity_type == CONNECTIVITY_TYPE_GRID:
+        connectivity = grid_to_graph(data[0].shape[0], 1)
+    else:
+        connectivity = kneighbors_graph(data.T, number_of_features - 1, include_self=False)
+
+    agglo = FeatureAgglomeration(connectivity=connectivity, n_clusters=number_of_features, affinity=affinity, compute_distances=True).fit(data)
+    transformed_data = agglo.transform(data)
+    tranformations.append(('agglomeration', agglo))
+
+    tranformations_pipe = Pipeline(tranformations)
+
+    components_name = [f"col{index}" for index in range(0, number_of_features)]
+    df = pd.DataFrame(data = transformed_data, columns = components_name)
+
+    # Save outputs
+    save_data_frame_to_directory(output_dataset, df)
+    save_pickle_transform_to_directory(output_model, tranformations_pipe)
+    plot_dendrogram(agglo, 'outputs/plot.png')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("extract-pca")
+    parser.add_argument("--dataset", dest="input_dataset", required=True, type=str, help="Input dataset")
+    parser.add_argument("--number-of-features", dest="number_of_features", type=int, help="Number of dimensions to reduce to", required=True)
+    parser.add_argument("--normalize", dest="normalize", type=bool, help="Whether or not to normalize data to zero mean", required=True, default=True)
+    parser.add_argument("--connectivity-type", dest="connectivity_type", choices=CONNECTIVITY_TYPE, default=CONNECTIVITY_TYPE_NONE)
+    parser.add_argument("--affinity", dest="affinity", choices=AFFINITY, default=AFFINITY_EUCLIDEAN)
+    parser.add_argument("--output-dataset", dest="output_dataset", type=str, help="Transformed dataset")
+    parser.add_argument("--output-model", dest="output_model", type=str, help="Trained model")
+    args = parser.parse_args()
+
+    RunModule(**vars(args))
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,9 +1,11 @@
+from pathlib import Path
 import numpy as np
 import shutil
 from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, DataFrameDirectory
 from azureml.studio.core.io.transformation_directory import PickleTransformationDirectory
 
 from modules.aml_extract_pca.extract_pca import RunModule as extract_pca
+from modules.aml_apply_transformation.apply_transformation import RunModule as apply_transformation
 
 def test_pca_matches_shapes():
     args = {
@@ -32,3 +34,33 @@ def test_pca_matches_shapes():
     shutil.rmtree(args['output_model'])
     shutil.rmtree(args['output_dataset'])
     shutil.rmtree(args['output_eigenvectors'])
+
+def test_pca_apply_transform():
+    args = {
+        'input_dataset': 'modules/samples_data/dataset',
+        'number_of_dimensions': 5,
+        'normalize': True,
+        'solver': 'randomized',
+        'output_dataset': 'modules/samples_data/transformed',
+        'output_model': 'modules/samples_data/transform',
+        'output_eigenvectors': 'modules/samples_data/eigen'
+    }
+
+    transform_output = 'modules/samples_data/transform_transformed'
+
+    extract_pca(**args)
+    apply_transformation(args['input_dataset'], args['output_model'], transform_output)
+
+    output_data = load_data_frame_from_directory(args['output_dataset']).data
+    transform_output_data = load_data_frame_from_directory(transform_output).data
+
+    assert(transform_output_data.shape[-1] == args['number_of_dimensions'])
+    assert(transform_output_data.shape[0] == output_data.shape[0])
+
+    if args['normalize']:
+        assert(np.all(np.isclose(transform_output_data.mean(axis=0), 0)))
+    
+    shutil.rmtree(args['output_model'])
+    shutil.rmtree(args['output_dataset'])
+    shutil.rmtree(args['output_eigenvectors'])
+    shutil.rmtree(transform_output)
