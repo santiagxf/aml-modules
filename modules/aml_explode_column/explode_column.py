25212626797a36222b1153334dd5cb7e9a097165
santiagxf <santiagof@outlook.com>
2021-10-14 11:47:07 -0400

    feat: module explode

--- modules/aml_explode_column/explode_column.py
+++ modules/aml_explode_column/explode_column.py
@@ -1,48 +1,43 @@
 import argparse
+import pandas as pd
+import numpy as pd
+import itertools
 from typing import Union
 
 from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
 
-SPLIT_MODE_COLUMNS = 'Into columns'
-SPLIT_MODE_ROWS = 'Into rows'
-SPLIT_MODE_ARRAY = 'As array-like'
-SPLIT_MODE = [SPLIT_MODE_COLUMNS, SPLIT_MODE_ROWS, SPLIT_MODE_ARRAY]
+EXPLODE_MODE_COLUMNS = 'Into columns'
+EXPLODE_MODE_ROWS = 'Into rows'
+EXPLODE_MODE = [EXPLODE_MODE_COLUMNS, EXPLODE_MODE_ROWS]
 
-def RunModule(input_dataset: str, column_name: str, split_mode: str, split_by: str, output_dataset: str, new_columns_name:Union[str, None]=None):
+def RunModule(input_dataset: str, column_name: str, explode_mode: str, output_dataset: str, new_columns_name:Union[str, None]=None):
     data_folder = load_data_frame_from_directory(input_dataset)
     
     # Check if column is available
     if data_folder.schema:
         column_type = data_folder.get_column_type(column_name)
-        if column_type != 'String':
-            raise TypeError(f'Column {column_name} is not of type String, but {column_type}. Type is not compatible.')
+        if column_type != 'Object':
+            raise TypeError(f'Column {column_name} is not of type Object, but {column_type}. Type is not compatible.')
     else:
         print('[DEBUG] Column type checking ignored as schema not available.')
 
-    if not split_by:
-        print('[DEBUG] Setting split_by to a white space.')
-        split_by = ' '
-
     data = data_folder.data
     if len(data.index) > 0:
-        if split_mode == SPLIT_MODE_COLUMNS:
+        if explode_mode == EXPLODE_MODE_COLUMNS:
             # Calculate number of columns to use
             if new_columns_name:
                 columns_name = [col.strip() for col in new_columns_name.split(',')]
             else:
-                # Hint: this may be too much
-                max_count = data[column_name].str.split(split_by).len().max()
+                max_count = data[column_name].map(len).max()
                 columns_name = [f'{column_name}_{index}' for index in range(0, max_count)]
             
-            data[columns_name] = data[column_name].str.split(split_by, expand=True)
+            padded_data = np.array(list(itertools.zip_longest(*data[column_name].tolist(), fillvalue=None))).T
+            data[columns_name] = pd.DataFrame(padded_data, index=data.index)
 
-        elif split_mode == SPLIT_MODE_ROWS or split_mode == SPLIT_MODE_ARRAY:
-            data[column_name] = data[column_name].apply(lambda x: x.split(split_by))
-        
-            if split_mode == SPLIT_MODE_ROWS:
-                data = data.explode(column_name)
+        elif explode_mode == EXPLODE_MODE_ROWS:
+            data = data.explode(column_name)
         else:
-            raise ValueError(f'{split_mode} is not a valid split mode')
+            raise ValueError(f'{explode_mode} is not a valid explode mode')
     else:
         print('[DEBUG] Nothing to do as dataframe is empty.')
 
@@ -52,8 +47,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("aml-module")
     parser.add_argument("--dataset", dest="input_dataset", type=str, required=True)
     parser.add_argument("--column-name", dest="column_name", type=str, required=True)
-    parser.add_argument("--split-by", dest="split_by", type=str, required=False, default=' ')
-    parser.add_argument("--split-mode", dest="split_mode", type=str, choices=SPLIT_MODE, default=SPLIT_MODE_ARRAY)
+    parser.add_argument("--explode-mode", dest="explode_mode", type=str, choices=EXPLODE_MODE, default=EXPLODE_MODE_ROWS)
     parser.add_argument("--output-dataset", dest="output_dataset", type=str)
     parser.add_argument("--new-columns-name", dest="new_columns_name", type=str, Required=False)
     args = parser.parse_args()
