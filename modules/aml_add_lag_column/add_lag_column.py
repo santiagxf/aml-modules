19ada6bc78ea50e1d1198050ebc1e4c0fd958453
santiagxf <santiagof@outlook.com>
2021-10-15 13:15:21 -0400

    fix: chaning the order of columns

--- modules/aml_add_lag_column/add_lag_column.py
+++ modules/aml_add_lag_column/add_lag_column.py
@@ -4,10 +4,11 @@ from typing import Union
 from azureml.studio.core.io.data_frame_directory import load_data_frame_from_directory, save_data_frame_to_directory
 
 
-def RunModule(input_dataset: str, column_name: str, lag_columns: int, lag_by: int, output_dataset: str, drop_na: bool=True):
+def RunModule(input_dataset: str, column_name: str, lag_columns: int, lag_by: int, output_dataset: str, drop_nulls: bool=True):
     data_folder = load_data_frame_from_directory(input_dataset)
     
     if ',' in column_name:
+        print('[DEBUG] Multiple columns has been indicated. Identifying them...')
         column_names = [col.strip() for col in column_name.split(',')]
     else:
         column_names = [column_name]
@@ -23,12 +24,13 @@ def RunModule(input_dataset: str, column_name: str, lag_columns: int, lag_by: in
         raise ValueError("Parameter lag_by is incorrect. It has to be different of zero")
 
     data = data_folder.data
-    for lag in range(1, lag_columns+1):
-        for column in column_names:
+    for column in column_names:
+        for lag in range(1, lag_columns+1):
             print(f'[DEBUG] Creating {column} with lag {lag}')
             data[f'{column}_lag{lag}'] = data[column].shift(lag*lag_by)
 
-    if drop_na:
+    if drop_nulls:
+        print(f'[DEBUG] Dropping nulls')
         data.dropna(inplace=True)
 
     save_data_frame_to_directory(output_dataset, data)
@@ -39,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("--column-name", dest="column_name", type=str, required=True)
     parser.add_argument("--lag-columns", dest="lag_columns", type=int, required=True)
     parser.add_argument("--lag-by", dest="lag_by", type=int, required=False, default=1)
+    parser.add_argument("--drop-nulls", dest="drop_nulls", type=bool, required=False, default=True)
     parser.add_argument("--output-dataset", dest="output_dataset", type=str)
     args = parser.parse_args()
 
